df87fd97c54ec9d07a2afe91140e45b06dc42876
Ahmed BenHajAmor <ahmed.benhajamorr@gmail.com>
2023-12-24 15:10:08 +0000

    changes

--- Portfolio/src/components/Home/Home.jsx
+++ Portfolio/src/components/Home/Home.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
-
+import React from 'react';
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
+import { IoIosSend } from "react-icons/io"
+import './Home.css';
 function Home() {
   return (
-    <section className='home'>
-      Home
+    
+    <section className='home-accounts-container'>
+      <div className="home-accounts">
+        <div className="accounts-icons">
+          <a href="#" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+          <a href="#" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+          <a href="#" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+        </div>
+      </div>
+
+      <div className="home-text-container">
+        <div className="home-text">
+          <div className="name-post">
+            <div className="name"><h1>Ahmed Ben Haj Amor</h1></div>
+            <div className="post"><h4>React Devoloper</h4></div>
+          </div>
+
+          <div className="discription">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi rem quis odio aliquid molestias iure libero qui</p>
+          </div>
+
+          <div className="sey-hello">
+            <a href="#">
+              <div className='sey-hello-text'>Sey Hello</div>
+              <div className="sey-hello-icon"><IoIosSend /></div>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="home-imag">
+
+      </div>
     </section>
   )
 }
 
-export default Home
+export default Home;
