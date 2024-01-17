636ab00a76308eb645abd341cb4a8204651cc44c
Ahmed BenHajAmor <ahmed.benhajamorr@gmail.com>
2024-01-17 07:42:10 +0000

    changes

--- Portfolio/src/components/Projects/ProjectsMainContent/ProjectCard.jsx
+++ Portfolio/src/components/Projects/ProjectsMainContent/ProjectCard.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './ProjectCard.css'
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
 
 function ProjectCard({title, image, github, demo}) {
   return (
     <div className="project-card">
-      <div className="project-card-img"><img src={image} alt="#" /></div>
+      <div className="project-card-img"><img src={image} alt="img" /></div>
       <div className="project-card-title super-dark-color"><span>{title}</span></div>
-      <div className="project-card-title normal-dark-color p-font-size">
-        hhh
+      <div className="project-card-footer normal-dark-color p-font-size">
+        <div className="project-demo"><a href=""><span>demo</span><FaRegArrowAltCircleRight /></a></div>
+        <div className="project-github"><a href=""><FaGithub /></a></div>
       </div>
     </div>
   )
