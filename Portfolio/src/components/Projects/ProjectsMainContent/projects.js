104c05ef5cc97fb87221006e5066e0465140c93b
Ahmed BenHajAmor <ahmed.benhajamorr@gmail.com>
2024-05-29 12:44:37 +0000

    changes

--- Portfolio/src/components/Projects/ProjectsMainContent/projects.js
+++ Portfolio/src/components/Projects/ProjectsMainContent/projects.js
@@ -1,5 +1,6 @@
 import nikeCloneHome from '../../../assets/nike-clone-home.webp'
 import weatherApp from '../../../assets/weather-app.webp'
+import taw9itSalat from '../../../assets/taw9it_salat.webp'
 
 
 export const projects = [
@@ -14,5 +15,11 @@ export const projects = [
         image: weatherApp,
         githubLink: 'https://github.com/Ahmed-BenHajAmor/Weather-application',
         demoLink: 'https://weather-application-reactjs.vercel.app/'
+    },
+    {
+        title: 'Taw9it salat',
+        image: taw9itSalat,
+        githubLink: 'https://github.com/Ahmed-BenHajAmor/Taw9it_salat',
+        demoLink: 'https://taw9it-salat.vercel.app/'
     },
 ]
